e24ea6622adfed0579ce2769cb0c66f7fd773d8a
Preston Grisham <prestongrisham@Prestons-27-iMac.local>
2017-01-17 10:46:12 -0500

    Finished Facebook Auth

--- Podfile
+++ Podfile
@@ -12,6 +12,7 @@ target 'devslopes-social' do
   pod 'Firebase/Database'
   pod 'Firebase/Messaging'
   pod 'Firebase/Storage'
-  pod 'SwiftKeychainWrapper' 
+  pod 'SwiftKeychainWrapper'
+  pod 'FBSDKLoginKit'
 
 end
--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,36 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookButtonTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
+            if error != nil {
+                print("CPG: Facebook Login error: \(error)")
+            }
+            else if result?.isCancelled == true {
+                print("CPG: User cancelled Facebook authentication")
+            }
+            else {
+                print("CPG: Sucessfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                print("CPG: Facebook Credential: \(credential)")
+                self.firebaseAuth(credential)
+                
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("CPG: Firebase authentication error: \(error)")
+            } else {
+                print("CPG: Sucessfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
