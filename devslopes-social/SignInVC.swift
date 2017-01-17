7daeba27688715fed41ba714571af88a62c68ecd
Preston Grisham <prestongrisham@Prestons-27-iMac.local>
2017-01-17 11:10:36 -0500

    Email Auth Complete

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -12,6 +12,10 @@ import FBSDKLoginKit
 import Firebase
 
 class SignInVC: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: LoginTextFields!
+    @IBOutlet weak var passwordTextField: LoginTextFields!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +27,31 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        
+        if let email = emailTextField.text , let password = passwordTextField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    //User Signed In
+                    print("CPG: User authenticated with email and password")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error == nil {
+                            print("CPG: User created with Firebase")
+                        } else {
+                            print("CPG: Error creating user: \(error)")
+                        }
+                    })
+                }
+            })
+        } else {
+            print("Need to fill out both fields")
+            //CREATE ALERT HERE FOR USERS
+        }
+        
+    }
+    
+    
     @IBAction func facebookButtonTapped(_ sender: Any) {
         
         let facebookLogin = FBSDKLoginManager()
